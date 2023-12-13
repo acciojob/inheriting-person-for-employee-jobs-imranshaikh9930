84a1a8a22bcd75863326fb48dccb5076d9563078
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-12-13 15:05:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function(){
-	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+
+	
 }
 function Employee(name, age, jobTitle) {
 
@@ -17,8 +19,7 @@ function Employee(name, age, jobTitle) {
 Employee.prototype = Object.create(Person.prototype);
 
 Employee.prototype.jobGreet = function(){
-	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
-}
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)}
 
 // Do not change code below this line
 window.Person = Person;
